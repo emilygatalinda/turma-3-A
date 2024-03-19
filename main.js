df5ad4f773b1032aa7592a3163f4011d20b471f1
emilygatalinda <127235902+emilygatalinda@users.noreply.github.com>
2024-03-19 15:11:26 -0300

    19/03

--- main.js
+++ main.js
@@ -16,5 +16,25 @@ for(let i=0;i <botoes.length;i++){
 
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2024-10-05");
+const tempoObjetivo2 = new Date("2024-12-05");
+const tempoObjetivo3 = new Date("2024-12-30");
+const tempoObjetivo4 = new Date("2025-01-02");
 
-contadores[0].textContent = tempoObjetivo1;
+if (tempoFinal > 0){
+    return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos";
+} else {
+    return "Prazo Finalizado";
+}
+
+const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
+
+function atualizaCronometro(){
+for (let i = 0; i < contadores.length; i++) {
+    contadores[i].textContent = calculaTempo(tempos[i]);
+}
+}
+function comecaCronometro(){
+atualizaCronometro();
+setInterval(atualizaCronometro, 1000);
+}
+comecaCronometro();
